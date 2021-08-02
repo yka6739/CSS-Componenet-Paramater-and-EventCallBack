c0122c4a5e01d0849ae9f859b4cfc3be9e3ab248
YATENDRA AWANA <yka6739@gmail.com>
2021-08-02 15:04:04 +0530

    Add project files.

---
+++ DouiMovie/Client/Shared/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DouiMovie.Client.Shared
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Poster { get; set; }
+    }
+}
